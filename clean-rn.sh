99ca7ba49c8dc38df56f5bdf6aa4252c3fee98b9
Rodrigo Prado Fontes <fontesr2@gmail.com>
2021-04-01 11:10:39 -0300

    add: script to clean RN projects

---
+++ clean-rn.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+getProjectRootDir() {
+  local originalDir=$(pwd)
+  local scriptPath=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+  local folders=$(echo $scriptPath | tr '/' "\n")
+
+  cd $originalDir
+
+  for folder in $folders
+  do
+    local packageFile=$(ls -la $scriptPath | grep -c package.json)
+
+    if [ $packageFile == '0' ]
+    then
+      # Go up one folder
+      scriptPath=$(echo $scriptPath | sed -E 's/(.*)\/.*/\1/g')
+    else
+      echo $scriptPath
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+projectRoot=$(getProjectRootDir)
+
+beautyEcho() {
+  echo "💄 $1"
+}
+
+cleanAndroid() {
+  local originalDir=$(pwd)
+  local androidPath=$projectRoot/android
+  cd $androidPath
+
+  # Continue execution if gradle exits with an error
+  set +e
+  ./gradlew clean &> /dev/null
+  # Stop execution on the next error
+  set -e
+
+  cd $originalDir
+
+  rm -rf \
+    $androidPath/.gradle \
+    $androidPath/.idea \
+    $androidPath/android.iml \
+    $androidPath/app/app.iml \
+    $androidPath/app/build \
+    $androidPath/belezanaweb.iml \
+    $androidPath/build \
+    $androidPath/local.properties
+}
+
+cleanAndroidNuclear() {
+  beautyEcho 'Make sure you have installed all dependencies listed here:'
+  beautyEcho 'https://github.com/rock3r/deep-clean#installing-the-script-dependencies'
+
+  local originalDir=$(pwd)
+
+  cd $projectRoot/android
+
+  curl \
+    'https://raw.githubusercontent.com/rock3r/deep-clean/master/deep-clean.kts' \
+    -G \
+    -s \
+    > deep-clean.kts
+
+  kscript deep-clean.kts --nuke
+
+  rm deep-clean.kts
+
+  cd $originalDir
+}
+
+cleanIos() {
+  local iosPath=$projectRoot/ios
+  rm -rf \
+    $iosPath/Pods \
+    $iosPath/Podfile.lock
+}
+
+cleanNode() {
+  rm -rf \
+    $projectRoot/node_modules \
+    $projectRoot/package-lock.json \
+    $projectRoot/yarn.lock
+}
+
+# TODO: make sure the script is being run from a React Native project
+
+case "$1" in
+  android)
+    cleanAndroid
+    ;;
+  android-nuclear)
+    cleanAndroidNuclear
+    ;;
+  ios)
+    cleanIos
+    ;;
+  node)
+    cleanNode
+    ;;
+  *)
+    cleanAndroid
+    cleanIos
+    cleanNode
+    ;;
+esac
