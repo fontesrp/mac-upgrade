41135f96f5e714a23ccc14df42ef44005c25bc89
Rodrigo Prado Fontes <fontesr2@gmail.com>
2021-04-01 10:31:54 -0300

    add: new scripts

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+gitHubUsername=fontesrp
+gitHubRepoName=mac-upgrade
+gitHubRepoPath=$gitHubUsername/$gitHubRepoName
+gitHubBranch=master
+
+downloadFile() {
+  curl \
+    "https://raw.githubusercontent.com/$gitHubRepoPath/$gitHubBranch/$1" \
+    -s \
+    -G \
+    -H 'Accept: text/plain'
+}
+
+getRepoTree() {
+  curl \
+    "https://api.github.com/repos/$gitHubRepoPath/git/trees/$gitHubBranch" \
+    -s \
+    -G \
+    -H 'Accept: application/vnd.github.v3+json' \
+    2>&1
+}
+
+getScriptsFromJsonTree() {
+  local parseJson="
+    console.log(
+      JSON.parse(process.argv[1])
+        .tree.map(({ path }) => \`\${path}\`)
+        .filter(path => path.endsWith('.sh') && !path.startsWith('install'))
+        .join('\\n')
+    )
+  "
+  node -e "$parseJson" "$1"
+}
+
+saveFileAsScript() {
+  local filename=$1
+  local scriptName=`echo $filename | sed -E 's/([[:alpha:]]+)\.sh/\1/g'`
+  local scriptPath=/usr/local/bin/$scriptName
+  rm -f "$scriptPath"
+  downloadFile $filename > $scriptPath
+}
+
+repoTree=`getRepoTree`
+
+for file in `getScriptsFromJsonTree "$repoTree"`
+do
+  saveFileAsScript $file
+done
---
+++ rockstar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+open /Applications/Slack.app
+open /Applications/Microsoft\ Teams.app
+open /Applications/Harvest.app
+sublime
+upgrade
